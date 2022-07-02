cbcbac13a4d926e853cc3ed29f1bda20b2b9ae95
Elias Kouskoumvekakis <eliaskousk@gmail.com>
2022-07-02 05:26:42 +0300

    Use a subset of train images and the previous transforms

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
+from torch.utils.data import Subset
 
 import torchvision
 import torchvision.transforms as transforms
@@ -31,7 +32,7 @@ def main():
     )
 
     parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
-    parser.add_argument('--model', default="vgg11", type=str, help='model')
+    parser.add_argument('--model', default="resnet18", type=str, help='model')
     parser.add_argument('--epochs', default=200, type=int, help='epochs')
     parser.add_argument('--bs', default=128, type=int, help='batch size')
     parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
@@ -80,51 +81,52 @@ def main():
 
     # Data
     print('==> Preparing data..')
-    transform_train = transforms.Compose([
-        transforms.RandomCrop(32, padding=4),
-        transforms.RandomHorizontalFlip(),
-        transforms.ToTensor(),
-        transforms.Normalize((0.4914, 0.4822, 0.4465),
-                             (0.2023, 0.1994, 0.2010)),
-    ])
+    # transform_train = transforms.Compose([
+    #     transforms.RandomCrop(32, padding=4),
+    #     transforms.RandomHorizontalFlip(),
+    #     transforms.ToTensor(),
+    #     transforms.Normalize((0.4914, 0.4822, 0.4465),
+    #                          (0.2023, 0.1994, 0.2010)),
+    # ])
 
-    # transform_train = transforms.Compose(
-    #     [
-    #         transforms.ToTensor(),
-    #         transforms.ToPILImage(),
-    #         transforms.Pad(4, padding_mode="reflect"),
-    #         transforms.RandomCrop(32),
-    #         transforms.RandomHorizontalFlip(),
-    #         transforms.ToTensor(),
-    #         transforms.Normalize(
-    #             mean=[x / 255.0 for x in [125.3, 123.0, 113.9]],
-    #             std=[x / 255.0 for x in [63.0, 62.1, 66.7]],
-    #         ),
-    #     ]
-    # )
+    transform_train = transforms.Compose(
+        [
+            transforms.ToTensor(),
+            transforms.ToPILImage(),
+            transforms.Pad(4, padding_mode="reflect"),
+            transforms.RandomCrop(32),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize(
+                mean=[x / 255.0 for x in [125.3, 123.0, 113.9]],
+                std=[x / 255.0 for x in [63.0, 62.1, 66.7]],
+            ),
+        ]
+    )
 
-    transform_test = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.4914, 0.4822, 0.4465),
-                             (0.2023, 0.1994, 0.2010)),
-    ])
+    # transform_test = transforms.Compose([
+    #     transforms.ToTensor(),
+    #     transforms.Normalize((0.4914, 0.4822, 0.4465),
+    #                          (0.2023, 0.1994, 0.2010)),
+    # ])
 
-    # transform_test = transforms.Compose(
-    #     [
-    #         transforms.ToTensor(),
-    #         transforms.Normalize(
-    #             mean=[x / 255.0 for x in [125.3, 123.0, 113.9]],
-    #             std=[x / 255.0 for x in [63.0, 62.1, 66.7]],
-    #         ),
-    #     ]
-    # )
+    transform_test = transforms.Compose(
+        [
+            transforms.ToTensor(),
+            transforms.Normalize(
+                mean=[x / 255.0 for x in [125.3, 123.0, 113.9]],
+                std=[x / 255.0 for x in [63.0, 62.1, 66.7]],
+            ),
+        ]
+    )
 
     trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
-    trainloader = torch.utils.data.DataLoader(trainset, **dataloader_kwargs)
-
-    testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
     dataloader_kwargs["shuffle"] = False
-    testloader = torch.utils.data.DataLoader(testset, **dataloader_kwargs)
+    trainloader = torch.utils.data.DataLoader(Subset(trainset, range(0, 40000)), **dataloader_kwargs)
+
+    testset = torchvision.datasets.CIFAR10(root='./data', train=True, download=False, transform=transform_test)
+    dataloader_kwargs["shuffle"] = False
+    testloader = torch.utils.data.DataLoader(Subset(testset, range(40000, 50000)), **dataloader_kwargs)
 
     # Model
     print(f'==> Building model {args.model}..')
